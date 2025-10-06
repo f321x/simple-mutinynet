274612698bee035d16f3bf656d89de5fc8b3be5f
f321x <f@f321x.com>
2025-10-06 10:26:48 +0200

    e-x: update dockerfile and entry script
    do history compaction every 4 days as mutinynet has lots of blocks
    causing an integer overflow if the history isn't being compacted every
    couple weeks

--- electrumx/entry.sh
+++ electrumx/entry.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+set -e
+
 mv generate_ssl_cert.sh /data
 
 cd /data
 
 ./generate_ssl_cert.sh
 
-exec /root/.local/bin/electrumx_server
+while true; do
+    echo "Starting ElectrumX server..."
+    /root/.local/bin/electrumx_server &
+    SERVER_PID=$!
+
+    echo "ElectrumX server started with PID $SERVER_PID."
+    sleep 345600 # 4 days
+
+    echo "Stopping ElectrumX server..."
+    kill $SERVER_PID
+    wait $SERVER_PID 2>/dev/null || true
+
+    echo "Running electrumx_compact_history..."
+    /root/.local/bin/electrumx_compact_history
+
+    echo "Restarting..."
+done
+
